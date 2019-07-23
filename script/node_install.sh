e3c4668577607ae9cd24aa4d88fe5b3fe18522d2
Zied <abdellaoui.zied@hotmail.com>
2019-07-23 17:28:23 +0000

    adapting node install script

--- script/node_install.sh
+++ script/node_install.sh
@@ -4,12 +4,16 @@ set -eo pipefail
 
 sudo apt-get update && sudo apt-get install -y sphinxsearch mysql-client
 
+echo "Checking nvm installation"
+
 if [[ -f ~/.nvm/nvm.sh ]] ; then
     echo "NVM already installed"
 else
+    echo "Installing nvm ..."
     curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 fi
 
+echo "setting nvm"
 source ~/.nvm/nvm.sh
 
 nvm install "$NODE_VERSION"
